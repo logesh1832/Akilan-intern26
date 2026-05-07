47d9c6d8f8699dc4518635c7f66ba8c2bada482f
akilanselvaraj-fsd <akilanselvaraj10@gmail.com>
2026-05-07 17:33:58 +0530

    Add files via upload

---
+++ 07-05/intro.js
@@ -0,0 +1,22 @@
+console.log("Hello, welcome to the JavaScript introduction!");
+
+// This is a single-line comment in JavaScript
+
+/*
+This is a multi-line comment in JavaScript.
+You can write multiple lines of comments here.
+*/
+
+// Variables in JavaScript
+let name = "Alice"; // Using 'let' to declare a variable
+const age = 30; // Using 'const' to declare a constant variable
+
+console.log("Name:", name);
+console.log("Age:", age);
+
+// Functions in JavaScript
+function greet(person) {
+    return "Hello, " + person + "!";
+}
+
+console.log(greet(name)); // Calling the function with the variable 'name'
